f56846d564faa8edd39992653a8111e246e37451
JoyWu <joy.wu@kkday.com>
2018-12-12 12:04:25 +0800

    Add hw2.py

--- ch2/hw2.py
+++ ch2/hw2.py
@@ -10,15 +10,16 @@ Just like in queries, if the mutation field returns an object type, you can ask 
 print(len(paragraph))
 
 # Q2: 把文章裡 'GraphQL' 這個單字變全大寫, 其餘單字變全小寫
-
 change = paragraph.lower()
-print(change.replace('graphql' ,'GRAPHQL'))
+x = change.replace('graphql', 'GRAPHQL', 2)
+print(x)
 
 # Q3: 印出這個段落有多少個英文單字(不含標點符號與空白)
 split = paragraph.split()
 print(len(split))  # 列表元素个数
 
 # Q4: x = 1, y = 6, 把兩個變數的值交換, 讓他變成 x = 6, y = 1, 只能使用加減乘除, 不能使用第三個變數
-x = 1 + 5
-y = 6 - 5
+x = 1
+y = 6
+x, y = y, x
 print(x , y)
